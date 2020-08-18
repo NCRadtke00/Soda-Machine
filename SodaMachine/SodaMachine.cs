319a23b06a93e2cbea11218dfeae645496aaedbc
NCRadtke00 <raddnc@gmail.com>
2020-08-18 14:58:05 -0500

    added inventory & register list's to SodaMachine.cs

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -14,5 +14,20 @@ namespace SodaMachine
         {
 
         }
+        public void Inventory()
+        {
+            inventory = new List<Can>();
+            this.inventory.Add("RootBeer"); //(25, "RootBeer", .60)
+            this.inventory.Add("Cola"); //(30,"Cola", .35)
+            this.inventory.Add("Orange"); //(45, "Orange", .06)
+        }
+        public void Register()
+        {
+            register = new List<Coin>();
+            this.register.Add(20, "Quarters");
+            this.register.Add(10, "Dimes");
+            this.register.Add(20, "Nickle");
+            this.register.Add(50, "Penny");
+        }
     }
 }
