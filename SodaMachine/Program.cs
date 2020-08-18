90d7dd3ca81b360523910946759f0159a6465b05
NCRadtke00 <raddnc@gmail.com>
2020-08-18 14:36:51 -0500

    added simulation.runmachine to program.cs to allow machine to run for debugging

--- SodaMachine/Program.cs
+++ SodaMachine/Program.cs
@@ -10,7 +10,8 @@ namespace SodaMachine
     {
         static void Main(string[] args)
         {
-
+            Simulation simulation = new Simulation();
+            simulation.RunMachine();
         }
     }
 }
