e21e93054c57e29e2a74aafc40dec0329410905d
NCRadtke00 <raddnc@gmail.com>
2020-08-20 15:43:40 -0500

    wrote out how i want methods to run. getting coffee

--- SodaMachine/UserInterface.cs
+++ SodaMachine/UserInterface.cs
@@ -8,31 +8,39 @@ namespace SodaMachine
 {
     static class UserInterface
     {
+        public UserInterface()
+        {
+           
+        }
+        public static string PickASoda()
+        {
+            Console.WriteLine("Select a can of soda by entering the number assosiated with it: \n" + "--- ( 1 ) = Cola --- \n" + "--- ( 2 ) = OrangeSoda --- \n" + "--- ( 3 ) = RootBeer --- \n");
+            ConsoleKeyInfo thing = Console.ReadKey();
+            if (thing.Key == ConsoleKey.D1)
+            {
+                can = Cola 
+                Console.WriteLine(" You picked a nice refreshing Cola");
+                return "Cola";
+            }
+            else if (thing.Key == ConsoleKey.D2)
+            {
+                can = "OrangeSoda";
+                Console.WriteLine(" You picked cool can of OrangeSoda POP!!");
+                return "OrangeSoda";
+            }
+            else if (thing.Key == ConsoleKey.D3)
+            {
+                can = "RootBeer";
+                Console.WriteLine(" You picked a nice cold RootBeer!");
+                return "RootBeer";
+            }
+        }
+        public void WelcomeScreen()
+        {
+            Console.WriteLine("Welcome to Quick Can vending machines!");
+            Console.WriteLine("All products are cold, refreshing and garented fresh!");
 
-        //public static string PickASoda()
-        //{
-        //    Console.WriteLine("Select a can of soda by entering the number assosiated with it: \n" + "--- ( 1 ) = Cola --- \n" + "--- ( 2 ) = OrangeSoda --- \n" + "--- ( 3 ) = RootBeer --- \n");
-        //    ConsoleKeyInfo thing = Console.ReadKey();
-        //    if (thing.Key == ConsoleKey.D1)
-        //    {
-        //        can = "Cola";
-        //        Console.WriteLine(" You picked a nice refreshing Cola");
-        //        return "Cola";
-        //    }
-        //    else if (thing.Key == ConsoleKey.D2)
-        //    {
-        //        can = "OrangeSoda";
-        //        Console.WriteLine(" You picked cool can of OrangeSoda POP!!");
-        //        return "OrangeSoda";
-        //    }
-        //    else if (thing.Key == ConsoleKey.D3)
-        //    {
-        //        can = "RootBeer";
-        //        Console.WriteLine(" You picked a nice cold RootBeer!");
-        //        return "RootBeer";
-        //    }
-        //}
-
+        }
         //public static string BuyASoda()
         //{
 
