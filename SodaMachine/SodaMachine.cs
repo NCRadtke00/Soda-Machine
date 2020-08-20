75561581f38863cadaf09b42088fc31dcafc3b25
NCRadtke00 <raddnc@gmail.com>
2020-08-20 14:32:23 -0500

    added coins to wallet

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -19,22 +19,22 @@ namespace SodaMachine
         }
         public void StockCoinsInSodaMachine()
         {
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Quarter quarter = new Quarter();
                 register.Add(quarter);
             }
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Dime dime = new Dime();
                 register.Add(dime);
             }
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Nickle nickle = new Nickle();
                 register.Add(nickle);
             }
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 50; i++)
             {
                 Penny penny = new Penny();
                 register.Add(penny);
--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -12,7 +12,31 @@ namespace SodaMachine
         Card card;
         public Wallet()
         {
-
+            StockCoinsInWallet();
+            coins = new List<Coin>();
+        }
+        public void StockCoinsInWallet()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Quarter quarter = new Quarter();
+                coins.Add(quarter);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Dime dime = new Dime();
+                coins.Add(dime);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Nickle nickle = new Nickle();
+                coins.Add(nickle);
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                Penny penny = new Penny();
+                coins.Add(penny);
+            }
         }
     }
 }
